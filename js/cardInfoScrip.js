6837f8db9b93ca746709f9310720cdc052564d43
Mooton PK <mootonpk@Mootons-MacBook-Air.local>
2022-12-28 17:05:46 +0700

    have to save cards data to sessionStorage

--- js/cardInfoScrip.js
+++ js/cardInfoScrip.js
@@ -1,7 +1,28 @@
+//parameter
+const cards = sessionStorage.getItem("cards");
+
+//HTML element
+const setName = sessionStorage.getItem("name");
+const table = document.getElementsByClassName("table-users")[0];
+
+//load & edite element
 document.body.onload = addElement();
 
 function addElement(){
 	//edite
-	document.getElementById("name").innerText = sessionStorage.getItem("name");
+	document.getElementById("name").innerText = setName;
 	document.getElementById("caption").innerText = sessionStorage.getItem("caption");
+	console.log(cards);	
+	cards.forEach((card)=>{console.log(card.data())});
+
+}
+
+function addDataInCard(card){
+	const row = table.insertRow(-1);
+	const wordCol = row.insertCell(0);
+	const meaningCol = row.insertCell(1);
+	
+	wordCol.innerHTML = card.data().word;
+	meaningCol.innerHTML = card.data().meaning;
+	console.log(card.data().word, card.data().meaning);
 }
--- js/mainScrip.js
+++ js/mainScrip.js
@@ -1,43 +1,76 @@
-const list = [["1", "name1", "cap1"],["2","name2","cap2"]];
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import {getFirestore, collection, getDocs, doc} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyApNoKK9h-7I7qDtij_pEp6z8tBml9I0R4",
+  authDomain: "web-word-7636a.firebaseapp.com",
+  projectId: "web-word-7636a",
+  storageBucket: "web-word-7636a.appspot.com",
+  messagingSenderId: "335174585981",
+  appId: "1:335174585981:web:92bd3998d5acb39b3d2b14"
+};
+
+//initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+//get data from database
+const sets  = await getData(db);
+
+//page path
 const _page2 = "html/cardInfo.html";
 
-document.body.onload = addElement(list) 
+document.body.onload = addElement(); 
 document.addEventListener("click", handleClick);
 
-function handleClick(event){
-	if (event.target.parentNode.className == "card"){
-		const card = event.target.parentNode;
+async function getData(db){
+	const admin = collection(db, "admin");
+	const sets = await getDocs(admin);
+
+	return sets;
+}
+
+async function handleClick(event){
+	if (event.target.parentNode.className == "set"){
+		const set = event.target.parentNode;
+		//save set name & caption to sessionStorage
 		for (let i = 0; i < 2; i++){
-			let k = card.children[i].className;
-			let v = card.children[i].innerText;
+			let k = set.children[i].className;
+			let v = set.children[i].innerText;
 
 			sessionStorage.setItem(k, v);
 		}
+		//save set data to sessionStorage
+		const setDoc = doc(db, "admin", sessionStorage.getItem("name"));
+		const cardsCol =  collection(setDoc, "cards");
+		const cards = await getDocs(cardsCol);
+		sessionStorage.setItem("cards", cards);
 		window.open(_page2, "_self");
+
 
 	}
 }
 
-function addElement(list){
-	for (let i = 0; i < list.length; i++){
-		const newCard = document.createElement("div");
-		newCard.className = "card";
-		newCard.id = list[i][0];
+function addElement(){
+	sets.forEach((set)=>{
+		const newSet = document.createElement("div");
+		newSet.className = "set";
+		newSet.id = set.id;
 
 		const newName = document.createElement("h1");
 		newName.className = "name";
-		newName.innerText = list[i][1];
+		newName.innerText = set.id;
 
 		const newCaption = document.createElement("p");
 		newCaption.className = "caption";
-		newCaption.innerText = list[i][2];
+		newCaption.innerText = set.data().caption;
 
-		newCard.appendChild(newName);
-		newCard.appendChild(newCaption);
+		newSet.appendChild(newName);
+		newSet.appendChild(newCaption);
 
 		const container = document.getElementById("container");
-		container.appendChild(newCard);
-	}
+		container.appendChild(newSet);
+	});
 
 }
 
